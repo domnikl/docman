07f71175188318c10f8701ec7f166af8b993e589
Dominik Liebler <dominik.liebler@flyeralarm.com>
2021-04-22 12:46:45 +0200

    pageing for PDfs in preview

--- src/components/PdfPreview.tsx
+++ src/components/PdfPreview.tsx
@@ -18,13 +18,26 @@ export default function PdfPreview(props: PdfPreviewProps) {
   const [numPages, setNumPages] = useState(0);
   const [pageNumber, setPageNumber] = useState(1);
 
+  const handleClick = (e) => {
+    const width = e.nativeEvent.target.width;
+    const offsetX = e.nativeEvent.offsetX;
+
+    if (offsetX >= width / 2 && pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    } else if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
   return (
     <div>
       <Document
         file={filePath}
-        onLoadSuccess={(success: DocumentLoadSuccess) =>
-          setNumPages(success.numPages)
-        }
+        onClick={handleClick}
+        onLoadSuccess={(success: DocumentLoadSuccess) => {
+          setNumPages(success.numPages);
+          setPageNumber(1);
+        }}
       >
         <Page pageNumber={pageNumber} />
       </Document>
