39520331334542c35d57d7ad41d91bb9c1cd05ed
Dominik Liebler <liebler.dominik@gmail.com>
2021-05-20 15:57:04 +0200

    make pagination more intuitive by using icons

--- src/components/PdfPreview.tsx
+++ src/components/PdfPreview.tsx
@@ -18,30 +18,90 @@ export default function PdfPreview(props: PdfPreviewProps) {
   const [numPages, setNumPages] = useState(0);
   const [pageNumber, setPageNumber] = useState(1);
 
-  const handleClick = (e) => {
-    console.log(e);
-    const width = e.nativeEvent.target.width;
-    const offsetX = e.nativeEvent.offsetX;
-
-    if (offsetX >= width / 2 && pageNumber < numPages) {
+  const nextPage = () => {
+    if (pageNumber <= numPages) {
       setPageNumber(pageNumber + 1);
-    } else if (pageNumber > 1) {
+    }
+  };
+
+  const previousPage = () => {
+    if (pageNumber > 1) {
       setPageNumber(pageNumber - 1);
     }
   };
 
   return (
     <div>
-      <Document
-        file={filePath}
-        onClick={handleClick}
-        onLoadSuccess={(success: DocumentLoadSuccess) => {
-          setNumPages(success.numPages);
-          setPageNumber(1);
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "space-evenly",
         }}
       >
-        <Page pageNumber={pageNumber} />
-      </Document>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            height: "calc(100vh - 5rem)",
+            width: "30px",
+            paddingRight: "5px",
+            cursor: "pointer",
+          }}
+          onClick={previousPage}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
+            />
+          </svg>
+        </div>
+        <Document
+          file={filePath}
+          onLoadSuccess={(success: DocumentLoadSuccess) => {
+            setNumPages(success.numPages);
+            setPageNumber(1);
+          }}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            height: "calc(100vh - 5rem)",
+            width: "30px",
+            cursor: "pointer",
+            paddingLeft: "5px",
+          }}
+          onClick={nextPage}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </div>
+      </div>
+
       <p>
         Page {pageNumber} of {numPages}
       </p>
